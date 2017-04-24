07dd7d84d5740fe3b8edb05a8ea3e12e75ad96cd
xidianlina <1198827599@qq.com>
2017-04-24 17:27:05 +0800

    first commit

---
+++ nine4.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+double test(double, double);
+int main(void)
+{
+	double x, y;
+	printf("Please enter two numbers \n");
+	scanf("%lf %lf", &x, &y);
+	printf("1/((1/x+1/y)/2) = %.3lf\n", test(x, y));
+
+	return 0;
+}
+
+double test(double x, double y)
+{
+	return 1 / ((1 / x + 1 / y) / 2);
+}
