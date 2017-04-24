4569f7dca048fd1dc4f021f742ea5cbb5a228ea5
xidianlina <1198827599@qq.com>
2017-04-24 21:48:35 +0800

    Revert "1-9"
    This reverts commit 43cb9247bc902c052b4b86d8dbb20ba9102542d5.

--- nine11.c
+++ nine11.c
@@ -1,37 +1,37 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//long Fibonacci(int);
-//int main(void)
-//{
-//	int n;
-//
-//	printf("Enter an integer (q to quit)\n");
-//	while (scanf("%d", &n) == 1)
-//	{
-//		printf("The term %d of Fibonacci sequence is %d\n", n, Fibonacci(n));
-//		printf("Enter an integer (q to quit)\n");
-//	}
-//	printf("Done.\n");
-//
-//	return 0;
-//}
-//
-//long Fibonacci(int n)
-//{
-//	int res, pre, temp, i;
-//	if (n > 2)
-//	{
-//		for (pre = 1, res = 1, i = 3; i <= n; i++)
-//		{
-//			temp = res + pre;
-//			pre = res;
-//			res = temp;
-//		}
-//	}
-//	else
-//	{
-//		res = 1;
-//	}
-//
-//	return res;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+long Fibonacci(int);
+int main(void)
+{
+	int n;
+
+	printf("Enter an integer (q to quit)\n");
+	while (scanf("%d", &n) == 1)
+	{
+		printf("The term %d of Fibonacci sequence is %d\n", n, Fibonacci(n));
+		printf("Enter an integer (q to quit)\n");
+	}
+	printf("Done.\n");
+
+	return 0;
+}
+
+long Fibonacci(int n)
+{
+	int res, pre, temp, i;
+	if (n > 2)
+	{
+		for (pre = 1, res = 1, i = 3; i <= n; i++)
+		{
+			temp = res + pre;
+			pre = res;
+			res = temp;
+		}
+	}
+	else
+	{
+		res = 1;
+	}
+
+	return res;
+}
