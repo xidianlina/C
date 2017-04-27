f0dbd0a20e7075d499f370f5dd3f6d773693b952
xidianlina <1198827599@qq.com>
2017-04-27 12:34:40 +0800

    chapter eleven

---
+++ eleventwo.c
@@ -0,0 +1,18 @@
+//#include <stdio.h>
+//int main(void)
+//{
+//	char note[] = "See you at the snack bar.";
+//	char *ptr;
+//
+//	ptr = note;
+//	printf("note = %s;&note = %p;value = %p\n", note, &note, note);
+//	printf("ptr = %s;&ptr = %p;value = %p\n", ptr, &ptr, ptr);
+//
+//	puts(ptr);
+//	puts(++ptr);
+//	note[7] = '\0';
+//	puts(note);
+//	puts(++ptr);
+//	
+//	return 0;
+//}
